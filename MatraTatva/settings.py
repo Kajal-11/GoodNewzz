edc84666d2c6148c5de64b2262ae18abcf6134cc
Kajal <kajal11122000@gmail.com>
2020-11-21 16:41:10 +0530

    email settings updated

--- MatraTatva/settings.py
+++ MatraTatva/settings.py
@@ -132,8 +132,8 @@ LOGIN_REDIRECT_URL = 'dashboard'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'  
 EMAIL_HOST = 'smtp.gmail.com' 
-EMAIL_HOST_PASSWORD = 'your-password'  
-EMAIL_HOST_USER = 'your-email-id'  
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')  
+EMAIL_HOST_USER = os.environ.get('EMAIL_ID')
 EMAIL_PORT = 587 
 EMAIL_USE_TLS = True
 
