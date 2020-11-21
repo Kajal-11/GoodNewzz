49815df73920d8accf17e76335448e3f24461597
Vanshika Gupta <vanshika125@gmail.com>
2020-11-21 15:59:25 +0530

    page updated

--- MatraTatva/settings.py
+++ MatraTatva/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('MATRATATVA_SECRET_KEY')
+SECRET_KEY = "3$!vd_6%fsrkuus^=%^!4jo8#yr6@_s_eigypn=(uo+!=ogo63"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
