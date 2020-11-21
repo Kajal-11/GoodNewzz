7d9fbe8fbb6b66e04f44b0cb80ac73cf4bf91c35
Kajal Goel <43801094+Kajal-11@users.noreply.github.com>
2020-11-22 01:10:39 +0530

    Update settings.py

--- MatraTatva/settings.py
+++ MatraTatva/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = "3$!vd_6%fsrkuus^=%^!4jo8#yr6@_s_eigypn=(uo+!=ogo63"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
